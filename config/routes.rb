467d8a701bffbec33a8e9c4a41ea8ae5063e7391
SamuelSHP <samuelhasjaputra@gmail.com>
2018-11-15 19:05:22 +0700

    Add User Controller, Model + Migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'user/index'
+  get 'user/new'
+  get 'user/edit'
+  get 'user/delete'
   get 'welcome/index'
 
-  resources :articles
+  # resources :articles
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
